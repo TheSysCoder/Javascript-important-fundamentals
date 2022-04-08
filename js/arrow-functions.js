4c69e77ee6c6e94e073cf0880105a907835244af
Darkhost <darkhost@Darkhosts-MacBook-Pro.local>
2022-04-08 20:07:37 +0530

    updates

---
+++ js/arrow-functions.js
@@ -0,0 +1,46 @@
+// arrow functions
+
+const myAge = (birthYear) => 2020 - birthYear;
+
+console.log(myAge(1994));
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(23, 67));
+
+// arrow function with one argument
+
+const greet = (yourName) =>
+  console.log(`Welcome ${yourName} to Javascript world!!!`);
+
+greet("Jake");
+
+// arrow functions as expression
+
+const age = 18;
+const we = age >= 18 ? () => console.log("Adult") : () => console.log("Baby");
+we();
+
+// multiline arrow function
+
+const sum = (a, b) => {
+  const result = a + b;
+  return result;
+};
+
+console.log(sum(34, 67));
+
+// practice all function in one
+
+function Person() {
+  this.name = "Jake";
+  this.job = "DevOps";
+
+  console.log(this.name);
+  this.say_welcome = function () {
+    console.log(`Welcome ${this.name}`);
+  };
+  say_welcome();
+}
+
+const x = new Person();
+x.say_welcome;
